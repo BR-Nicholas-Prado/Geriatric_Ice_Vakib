0b2baac05fc614a1965db3592111cc7babbd861e
Nicholas Prado <nick@getworkflo.com>
2021-02-12 18:09:27 -0600

    feat i210211 cli arg for root
    Able to consume a cli argument to operate some place other than the
    containing folder.
    Removes jars and replaces them with tombstones. NOTE this does not
    enforce a strict ordering of the folders yet, so this deleted the
    wrong jar and avoided an unneeded jar.

--- src/main/java/com/beyondrelations/fs/giv/Givakib.java
+++ src/main/java/com/beyondrelations/fs/giv/Givakib.java
@@ -2,26 +2,79 @@
 package com.beyondrelations.fs.giv;
 
 import java.io.IOException;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.FileTime;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.ProviderNotFoundException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class Givakib
 {
 
+	private FileSystem os;
+
+
 	public static void main(
 			String[] args
 	) {
-		/*
-		get the working root, or assume right here
-		
-		*/
+		try {
+			Givakib giv = new Givakib( FileSystems.getDefault() );
+			giv.replaceJarsWithTombstonesIn( args.length > 0
+					? args[ 0 ] : "" );
+		}
+		catch ( IllegalArgumentException | SecurityException
+				| FileSystemNotFoundException | ProviderNotFoundException any )
+		{
+			any.printStackTrace();
+		}
+	}
+
+
+	public Givakib(
+			FileSystem ls
+	) {
+		if ( ls == null )
+			throw new RuntimeException( "null filesystem is null" );
+		os = ls;
+	}
+
+
+	public void replaceJarsWithTombstonesIn(
+			String descriptionOfPath
+	) {
+		if ( descriptionOfPath.isEmpty() )
+			descriptionOfPath = ".";
+		try {
+			tombstonesInImmediateChildren( os.getPath( descriptionOfPath ) );
+		}
+		catch ( InvalidPathException ipe )
+		{
+			ipe.printStackTrace();
+			return;
+		}
+	}
+
+
+	private void tombstonesInImmediateChildren(
+			Path workingRoot
+	) {
 		try
 		{
-			Path workingRoot = Paths.get( "example" );
 			Stream<Path> allContents = Files.list( workingRoot );
 			Object[] allFilesInRoot = allContents.toArray();
+			Sculptor carver = new Sculptor();
 			int ind = 0, lim = allFilesInRoot.length -1; // leave the last one alone
 			for ( Object buh : allFilesInRoot )
 			{
@@ -30,14 +83,78 @@ public class Givakib
 				else
 					break;
 				Path path = (Path)buh;
-				System.out.println( path.toString() );
+				if ( Files.isDirectory( path ) )
+				{
+		System.out.println( "entering "+ path.toString() );
+					replaceJarsWithTombstonesIn( path, carver );
+				}
 			}
 		}
 		catch ( IOException ie )
 		{
 			ie.printStackTrace();
+			return;
 		}
 	}
+
+
+	private void replaceJarsWithTombstonesIn(
+			Path dirWithJarFiles, Sculptor replacesJars
+	) {
+		try
+		{
+			Stream<Path> allContents = Files.list( dirWithJarFiles );
+			allContents.forEach( replacesJars );
+		}
+		catch ( IOException ie )
+		{
+			ie.printStackTrace();
+			return;
+		}
+	}
+
+
+	/** because it carves tombstones */
+	private class Sculptor
+			implements Consumer<Path>
+	{
+
+		public void accept(
+				Path target
+		) {
+		System.out.println( "handed "+ target.toString() );
+			if ( ! target.getFileName().toString().endsWith( "jar" ) )
+				return;
+		System.out.println( "using it" );
+			/*
+			BasicFileAttributeView properties = Files.getFileAttributeView​(
+					target, BasicFileAttributeView.class );
+			*/
+			try {
+				FileTime creationTime = (FileTime)Files.getAttribute( target, "basic:creationTime" );
+				FileTime modifiedTime = (FileTime)Files.getAttribute( target, "basic:lastModifiedTime" );
+				// improve consider comparing creation with modified, in case I copied this in
+				LocalDate createdD = creationTime.toInstant().atZone( ZoneId.systemDefault() ).toLocalDate();
+				LocalDate modifiedD = modifiedTime.toInstant().atZone( ZoneId.systemDefault() ).toLocalDate();
+				LocalDate earlierD = createdD.compareTo( modifiedD ) < 1 ? createdD : modifiedD;
+				String tombstoneName = earlierD.toString() +"_"+ target.getFileName().toString()
+						.replace( "jar", "txt" );
+		System.out.println( "creating "+ tombstoneName );
+					Path intendedTombstone = target.getParent().resolve( os.getPath( tombstoneName ) );
+		System.out.println( "position "+ intendedTombstone.toString() );
+				Path ignored = Files.createFile( intendedTombstone );
+				Files.delete( target );
+			}
+			catch ( UnsupportedOperationException
+					| SecurityException | IOException | InvalidPathException ie )
+			{
+				ie.printStackTrace();
+				return;
+			}
+		}
+
+	}
+
 
 }
 
